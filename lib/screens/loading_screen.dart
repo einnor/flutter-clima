2c8a2d66f83ef015c85794744f9fe3c7a0569d7b
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-23 21:44:48 +0300

    Navigate to location screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
 
 const String apiKey = '439d4b804bc8187953eb36d2a8c26a02';
 
@@ -29,9 +30,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey';
     NetworkHelper networkHelper = NetworkHelper(url: url);
     var weatherData = await networkHelper.getData();
-    double temperature = weatherData['main']['temp'];
-    int condition = weatherData['weather'][0]['id'];
-    String cityName = weatherData['name'];
+//    double temperature = weatherData['main']['temp'];
+//    int condition = weatherData['weather'][0]['id'];
+//    String cityName = weatherData['name'];
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(),
+      ),
+    );
   }
 
   @override
