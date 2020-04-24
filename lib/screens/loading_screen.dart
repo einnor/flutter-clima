1d7e6a75ed34d2cc7603e2fad702b7cd483e4751
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-24 22:03:12 +0300

    Cleaning up

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -12,9 +12,6 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  double latitude;
-  double longitude;
-
   @override
   void initState() {
     super.initState();
@@ -24,11 +21,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    latitude = location.latitude;
-    longitude = location.longitude;
 
     final String url =
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
+        'https://api.openweathermap.org/data/2.5/weather?lat=$location.latitude&lon=$location.longitude&appid=$apiKey&units=metric';
     NetworkHelper networkHelper = NetworkHelper(url: url);
     var weatherData = await networkHelper.getData();
     print('Get data');
