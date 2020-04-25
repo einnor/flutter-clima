23dcd6650db6a006ccb633b456266a9c658bf445
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-25 15:24:34 +0300

    Border radius

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -45,6 +45,11 @@ class _CityScreenState extends State<CityScreen> {
                     hintStyle: TextStyle(
                       color: Colors.grey,
                     ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                    ),
                   ),
                 ),
               ),
