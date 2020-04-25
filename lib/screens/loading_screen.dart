bad5ac0c724c90d9e2ba5c074f4e30e670d774b8
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-25 13:23:40 +0300

    Cleaning up

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,6 +16,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
+    WeatherModel weatherModel = WeatherModel();
+    var weatherData = weatherModel.getLocationWeather();
+
     Navigator.push(
       context,
       MaterialPageRoute(
