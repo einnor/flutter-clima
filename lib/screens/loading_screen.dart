80b09e9c445080325ece847df47493d628f62158
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-25 13:27:15 +0300

    Update the details on button press

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -17,7 +17,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocationData() async {
     WeatherModel weatherModel = WeatherModel();
-    var weatherData = weatherModel.getLocationWeather();
+    var weatherData = await weatherModel.getLocationWeather();
 
     Navigator.push(
       context,
