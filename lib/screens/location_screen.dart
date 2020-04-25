2ab76f85f76e4d25aa15b9858e409ff2c27851cf
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-25 15:48:06 +0300

    Cleaning up

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -80,7 +80,11 @@ class _LocationScreenState extends State<LocationScreen> {
                           context,
                           MaterialPageRoute(
                               builder: (context) => CityScreen()));
-                      if (typedName != null) {}
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
                     },
                     child: Icon(
                       Icons.location_city,
